4f6457d4c2b94b3c48c7ceffd87e2f91bc34849f
goyalk130 <goyalkaran130@gmail.com>
2023-07-13 05:49:50 +0530

    next step navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,16 +10,14 @@ const Navbar = () => {
   return (
     <>
       <div
-        className={`${DmSans.className} font-medium sm:flex hidden justify-center items-center fixed h-[15vh] w-full top-0 left-0 my-0 px-20 grayscale-0 text-primary z-50`}
+        className={`${DmSans.className} font-medium md:flex hidden justify-center items-center fixed h-32 w-full top-0 left-0 my-0 px-20 grayscale-0 text-primary z-50 bg-text shadow-minimal shadow-sm `}
       >
-        <div className="w-full h-full grid grid-flow-row grid-cols-2">
-          <div className=" logo flex-grow flex justify-start items-center">
-            <h1 className="inline">
-              <img className="w-1/4" src="minimal.png" />
-            </h1>
+        <div className="hidden md:grid w-full h-28 grid-flow-row grid-cols-2">
+          <div className="relative h-full logo flex-grow flex justify-start items-center overflow-hidden">
+            <img className="relative w-1/5" src="minimal.png" />
           </div>
           {/* <div className=" logo flex-grow"></div> */}
-          <div className=" flex-grow flex flex-row justify-between items-center list-none p-0 m-0">
+          <div className="hideen md:flex flex-grow  flex-row justify-between items-center list-none p-0 m-0">
             <div className=" w-full flex flex-row justify-between items-center list-none p-0 m-0">
               <div className="relative flex-grow flex justify-center items-center  text-black ">
                 <h2 className="indivne-block">Home</h2>
@@ -37,6 +35,18 @@ const Navbar = () => {
           </div>
         </div>
       </div>
+      <div
+        className={`${DmSans.className} font-medium md:hidden flex justify-center items-center fixed w-full top-0 left-0 my-0 grayscale-0 text-primary z-50 bg-text shadow-minimal shadow-sm `}
+      >        
+        <div className="relative grid md:hidden w-full h-20 grid-flow-row grid-cols-1">
+          <div className="relative h-full logo flex-grow flex justify-start items-center overflow-hidden">
+            <img className="relative h-full" src="minimal.png" />
+          </div>
+          {/* <div className=" logo flex-grow"></div> */}
+        </div>
+      </div>
+
+
       <div className="sm:hidden  flex flex-grow flex-row  fixed h-16  justify-between   top-0 left-0 p-4">
         <div className="flex-grow">
           <div
@@ -54,7 +64,7 @@ const Navbar = () => {
       <div
         className={`${
           tooglenav ? "left-0" : "-left-full"
-        } sm:hidden z-40 w-full h-full absolute top-0 left-0`}
+        } md:hidden z-40 w-full h-full absolute top-0 left-0`}
         onClick={() => {
           settooglenav(!tooglenav);
           console.log(tooglenav);
@@ -63,7 +73,7 @@ const Navbar = () => {
       <div
         className={` ${
           tooglenav ? "left-0" : "-left-full"
-        } sm:hidden z-50 ease-in-out duration-200 fixed h-full   top-0 w-1/3 bg-amber-700 text-white bg-opacity-90 flex flex-col justify-between items-center list-none p-0 m-0`}
+        } md:hidden z-50 ease-in-out duration-200 fixed h-full   top-0 w-1/3 bg-amber-700 text-white bg-opacity-90 flex flex-col justify-between items-center list-none p-0 m-0`}
       >
         <div className=" w-full flex flex-col gap-4 justify-between items-start list-none p-0 m-0">
           <div className="relative flex-grow flex justify-center items-center   ">
--- src/components/reviewBox/ReviewBox.jsx
+++ src/components/reviewBox/ReviewBox.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const ReviewBox = ({review,name}) => {
   return (
-    <div className=' bg-highlight flex flex-col w-1/4 rounded-2xl  p-10'>
-            <div className='flex flex-col justify-start content-between h-full  gap-10 w-full'>
+    <div className='flex-grow bg-minimal flex flex-col w-full rounded-2xl p-3 lg:p-10 m-2'>
+            <div className='flex flex-col justify-start content-between h-full gap-4 lg:gap-6 w-full'>
                 <div className='flex inline w-1/4 rounded-full overflow-hidden'><img className='w-full h-fit' src="https://res.cloudinary.com/dk0qsqbcy/image/upload/v1687811504/DreamShaper_v6_a_background_full_of_colors_and_popping_digita_0_s5xmcz.webp" /></div>
-                <div className=' text-primary text-xl flex-grow'><h3>{review}</h3></div>
-                <div className=' text-primary text-2xl'><h2>{name}</h2></div>
+                <div className='w-48 sm:w-full text-primary text-xs lg:text-base flex-grow'><h3 >{review}</h3></div>
+                <div className=' text-primary text-base  lg:text-xl'><h2>{name}</h2></div>
             </div>
     </div>  
   )
